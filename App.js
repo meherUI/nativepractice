120b8d99b600595875f5e374bd2169e283d2c03c
Thatavarthi <ThataMeh@ES.AD.ADP.COM>
2020-08-25 15:57:40 +0530

    Refactor - Folder Structure

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SearchScreen from './screens/SearchScreen'
+import SearchScreen from './src/screens/SearchScreen'
 
 
 const Stack = createStackNavigator();
@@ -11,31 +11,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SearchScreen">
-        <Stack.Screen name="Business Search">
+        <Stack.Screen name="Business Searcher">
           {props => <SearchScreen {...props} extraData={'someData'} />}
         </Stack.Screen>        
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
