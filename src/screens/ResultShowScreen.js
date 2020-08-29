1a140613df599e1a47c0bb49eab1c0a5ca04dea9
Thatavarthi <ThataMeh@ES.AD.ADP.COM>
2020-08-29 17:16:54 +0530

    Result Details, useResults

---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultShowScreen = () => {
+    return (
+        <View>
+           <Text>ResultShow</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default ResultShowScreen;
